03d2c197ba465a647b77511613ec560bce55332d
Diego Cintra <diegosc2337@gmail.com>
2024-07-27 16:05:40 -0700

    chore: update 'TransactionResponseDTO'

--- src/main/java/com/transactionAssignment/app/dto/TransactionResponseDTO.java
+++ src/main/java/com/transactionAssignment/app/dto/TransactionResponseDTO.java
@@ -1,8 +1,10 @@
 package com.transactionAssignment.app.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TransactionResponseDTO {
     private String code;
 }
