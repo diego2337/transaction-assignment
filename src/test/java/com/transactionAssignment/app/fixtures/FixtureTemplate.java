7e8891b079f2e4758e48acb6f201d0228a6950a3
Diego Cintra <diegosc2337@gmail.com>
2024-07-27 16:06:09 -0700

    feat: create unit test for 'TransactionService'

--- src/test/java/com/transactionAssignment/app/fixtures/FixtureTemplate.java
+++ src/test/java/com/transactionAssignment/app/fixtures/FixtureTemplate.java
@@ -4,28 +4,68 @@ import br.com.six2six.fixturefactory.Fixture;
 import br.com.six2six.fixturefactory.Rule;
 import br.com.six2six.fixturefactory.loader.TemplateLoader;
 import com.transactionAssignment.app.dto.TransactionRequestDTO;
+import com.transactionAssignment.app.enums.CategoryStatusEnum;
+import com.transactionAssignment.app.model.Account;
+import com.transactionAssignment.app.model.AccountCategory;
+import com.transactionAssignment.app.model.Category;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.UUID;
 
 public class FixtureTemplate implements TemplateLoader {
     @Override
     public void load() {
         Fixture.of(TransactionRequestDTO.class).addTemplate("valid-food", new Rule(){{
-            add("id", UUID.randomUUID());
-            add("accountId", UUID.randomUUID());
-            add("amount", "234.56");
-            add("merchant", "Caju");
+            add("id", UUID.randomUUID().toString());
+            add("accountId", UUID.randomUUID().toString());
+            add("amount", 23.56f);
+            add("merchant", "Food");
             add("mcc", "5411");
         }});
 
         Fixture.of(TransactionRequestDTO.class).addTemplate("valid-meal", new Rule(){{
-            add("id", UUID.randomUUID());
-            add("accountId", UUID.randomUUID());
+            add("id", UUID.randomUUID().toString());
+            add("accountId", UUID.randomUUID().toString());
             add("amount", "234.56");
-            add("merchant", "Caju");
+            add("merchant", "Meal");
             add("mcc", "5811");
         }});
 
+        Fixture.of(AccountCategory.class).addTemplate("account-category-food", new Rule(){{
+//            add("account", one(Account.class, "account"));
+            add("category", one(Category.class, "category-food"));
+            add("totalAmount", 100.00f);
+        }});
+
+        Fixture.of(AccountCategory.class).addTemplate("account-category-meal", new Rule(){{
+//            add("account", one(Account.class, "account"));
+            add("category", one(Category.class, "category-meal"));
+            add("totalAmount", 100.00f);
+        }});
+
+        Fixture.of(Account.class).addTemplate("account", new Rule(){{
+            add("id", UUID.randomUUID().toString());
+            add("accountCategories", has(3).of(AccountCategory.class, "account-category-food", "account-category-meal", "account-category-meal"));
+            add("createdAt", LocalDateTime.now());
+            add("updatedAt", LocalDateTime.now());
+        }});
+
+        Fixture.of(Category.class).addTemplate("category-food", new Rule(){{
+            add("id", UUID.randomUUID().toString());
+            add("mcc", "5411");
+            add("category", CategoryStatusEnum.FOOD);
+            add("createdAt", LocalDateTime.now());
+            add("updatedAt", LocalDateTime.now());
+        }});
+
+        Fixture.of(Category.class).addTemplate("category-meal", new Rule(){{
+            add("id", UUID.randomUUID().toString());
+            add("mcc", "5811");
+            add("category", CategoryStatusEnum.MEAL);
+            add("createdAt", LocalDateTime.now());
+            add("updatedAt", LocalDateTime.now());
+        }});
     }
 }
--- src/test/java/com/transactionAssignment/app/service/TransactionServiceTest.java
+++ src/test/java/com/transactionAssignment/app/service/TransactionServiceTest.java
@@ -6,6 +6,9 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import com.transactionAssignment.app.dto.TransactionRequestDTO;
 import com.transactionAssignment.app.dto.TransactionResponseDTO;
+import com.transactionAssignment.app.model.Account;
+import com.transactionAssignment.app.model.AccountCategory;
+import com.transactionAssignment.app.repository.AccountRepository;
 import org.eclipse.jetty.http.HttpStatus;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.AfterEach;
@@ -13,24 +16,34 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class TransactionServiceTest {
-    @Mock
+    @InjectMocks
     TransactionService transactionService;
 
     @Mock
     AccountRepository accountRepository;
+
+    @Mock
+    Account account;
+
+    @Mock
+    AccountCategory accountCategory;
 
     @BeforeAll
     public static void setUp() {
@@ -40,9 +53,8 @@ public class TransactionServiceTest {
     @Test
     public void testExecuteFoodTransaction() {
         TransactionRequestDTO transactionRequestDTO = Fixture.from(TransactionRequestDTO.class).gimme("valid-food");
-        when(accountRepository.findById).thenReturn(Fixture.from(Account.class).gimme("account"));
-        when(accountRepository.findCategory).thenReturn(AccountCategory.class).gimme("account-category");
-        when(transactionService.hasEnoughCredit).thenReturn(true);
+        Account expectedAccount = Fixture.from(Account.class).gimme("account");
+        doReturn(Optional.of(expectedAccount)).when(this.accountRepository).findById(anyString());
 
         TransactionResponseDTO transactionResponseDTO = transactionService.execute(transactionRequestDTO);
 
